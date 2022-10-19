bcfcc55e6d314860669dec15fdcd321995e59919
Rashmi <rashmijayasekara00@gmail.com>
2022-10-19 16:04:18 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,3 @@
+// $('li').css('background-color', 'cyan').css('text-decoration', 'none').css('border', '1px solid black').css('align-items', 'center').css('justify-content', 'center').css('justify-content', 'space-around').css('padding', '5px').css('display', 'flex');
+// $('.mid').css('font-size','2rem').css('text-decoration','none');
+$('a').css('text-decoration','none');
